5d1dd706028a76cf18c031b75eec3e7c401ee6ad
xTCry (Vladislav Khr) <wolt6@mail.ru>
2020-09-15 17:20:56 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
+require('dotenv').config();
+
 import API from './api';
 import TTMan from './ttman';
 import './server';
-
-require('dotenv').config();
 
 const api = new API({
     login: process.env.LOGIN,
